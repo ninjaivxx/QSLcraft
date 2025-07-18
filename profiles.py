5bebdd8b29773c65ada95bb7be4cae72827d97d7
Ben Williams <desertblade@gmail.com>
2025-07-18 13:33:08 -0700

    Changing saving of profiles to an Application Folder on the OS system folders.

--- profiles.py
+++ profiles.py
@@ -1,7 +1,12 @@
 import os
 import json
+from appdirs import *
 
-PROFILE_PATH = os.path.join(os.path.dirname(__file__), "qsl_profiles.json")
+app_name = "QSLcraft"
+
+PROFILE_PATH = os.path.join(os.path.dirname(__file__), user_data_dir(app_name),"qsl_profiles.json")
+
+
 
 def load_profiles():
     if os.path.exists(PROFILE_PATH):
@@ -10,5 +15,7 @@ def load_profiles():
     return {}
 
 def save_profiles(profiles):
+    if not os.path.exists(os.path.dirname(PROFILE_PATH)):
+        os.makedirs(os.path.dirname(PROFILE_PATH))
     with open(PROFILE_PATH, 'w') as f:
         json.dump(profiles, f, indent=2)
